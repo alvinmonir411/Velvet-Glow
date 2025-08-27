e97dcea161f533345fc3f1f423f617e2eee9b06b
alvinmonir411 <moniruzzaman710@outlook.com>
2025-08-27 20:30:57 +0600

    setup with next js and done Navber and announcement

---
+++ src/app/Component/Announcement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Announcement = () => {
+  return (
+    <div>
+      <h1 className="flex justify-center items-center text-[16px] h-[48px] text-white bg-black">
+        30% OFF ALL ITEMS & FREE SHIPPING WITH CODE 'SPRING30" 1096 STORE CREDIT
+        ON FIRST ORDER
+      </h1>
+    </div>
+  );
+};
+
+export default Announcement;
